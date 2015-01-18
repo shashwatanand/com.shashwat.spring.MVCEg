2b298628d3d205748eb38aac1bd6cc8750462675
Shashwat Anand <shashwat.anand@gmail.com>
2015-01-18 13:12:11 +0530

    Add controlled  and model class

---
+++ src/main/java/com/shashwat/spring/User.java
@@ -0,0 +1,21 @@
+package com.shashwat.spring;
+
+public class User {
+	private String userName;
+
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+
+	/**
+	 * @param userName
+	 *            the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+}
